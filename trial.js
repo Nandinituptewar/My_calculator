561100c5fdb8dfe34e647d6bb850f6c44b0e977b
Nandinituptewar <51188252+Nandinituptewar@users.noreply.github.com>
2022-02-09 16:30:38 +0530

    Add files via upload

--- trial.js
+++ trial.js
@@ -1,12 +1,29 @@
 function expressionAdd(val){
 	document.getElementById('exp').value+=val;
 }
-function clearInput()
-{
+
+function clearInput(){
 	document.getElementById('exp').value="";
 }
+
 function calculateExpression(){
 	let expString=document.getElementById('exp').value;
 	let finalResult=eval(expString);
+	if(finalResult==undefined){
+		alert("Please enter the right input");
+		document.getElementById('exp').value="";
+	}
+	else{
 	document.getElementById('exp').value=finalResult;
+	history(expString,finalResult);
+	}
+}
+
+function history(expstring,finalresult){
+	historyString=expstring+"="+finalresult;
+	document.getElementById('history').innerHTML+=historyString+'<br/>';	
+}
+
+function deleteHistory(){
+	document.getElementById('history').innerHTML="";
 }
